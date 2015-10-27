4d7c9e70e0b2dfd6edb92aca87fb02b59a988cc1
Yuxi Pan <panyuxi@gmail.com>
2015-10-27 14:58:56 -0700

    add updateUDGraph, more clean-up

--- include/UDGraph.h
+++ include/UDGraph.h
@@ -24,16 +24,7 @@ public:
 
 		mTdiff = 999;
 	}
-
-	void Print() {
-		std::cout << " Date: " << mDate << std::endl;
-		std::cout << "t_diff: " << mTdiff << std::endl;
-		std::cout << "---------------SHA list: " << std::endl;
-		for (std::vector<std::string>::iterator it = mSha_list.begin();
-				it != mSha_list.end(); it++)
-			std::cout << (*it) << std::endl;
-
-	}
+	void Print();
 
 	int mTdiff;
 	std::vector<std::string> mSha_list;
@@ -74,22 +65,11 @@ public:
 		mUuid = "";
 		mGuid = "";
 		mFirstarc = NULL;
-
 	}
 
-	UNode(std::string uuid, std::string guid, ArcNode* firstarc) :
-			mUuid(uuid), mGuid(guid), mFirstarc(firstarc) {
-	}
-
-	UNode(const UNode& other) {
-		mUuid = other.mUuid;
-		mGuid = other.mGuid;
-		mFirstarc = other.mFirstarc;
-	}
-
-	void PrintData() {
-		std::cout << "UUID: " << mUuid << "GUID: " << mGuid << std::endl;
-	}
+	UNode(std::string uuid, std::string guid, ArcNode* firstarc);
+	UNode(const UNode& other);
+	void PrintData();
 
 	std::string mUuid;	//!< UUID/end-point id.
 	std::string mGuid;	//!< business group id.
@@ -103,36 +83,20 @@ public:
 	DNode() {
 		mDomain = "";
 		mIp = "";
+		mDate = "";
 		mReptool_tag = false;
 		mPre_score_sum = 0;
 		mPost_score_sum = 0;
 	}
 
-	DNode(std::string domain, std::string ip, bool reptool_tag, float pre_score,
-			float post_score) :
-			mDomain(domain), mIp(ip), mReptool_tag(reptool_tag), mPre_score_sum(
-					pre_score), mPost_score_sum(post_score) {
-	}
+	DNode(std::string domain, std::string ip, std::string date,
+			bool reptool_tag, float pre_score, float post_score);
+	DNode(const DNode& other);
+	void PrintData();
 
-	DNode(const DNode& other) {
-		mDomain = other.mDomain;
-		mIp = other.mIp;
-		mReptool_tag = other.mReptool_tag;
-		mPre_score_sum = other.mPre_score_sum;
-		mPost_score_sum = other.mPost_score_sum;
-	}
-
-	void PrintData() {
-		std::cout << "-----------------Domain-IP node: " << std::endl;
-		std::cout << "Domain: " << mDomain << std::endl;
-		std::cout << "IP: " << mIp << std::endl;
-		std::cout << "Reptool_tag: " << mReptool_tag << std::endl;
-		std::cout << "Pre_score_sum: " << mPre_score_sum << std::endl;
-		std::cout << "Post_score_sum: " << mPost_score_sum << std::endl;
-
-	}
 	std::string mDomain;
 	std::string mIp;
+	std::string mDate; //!< date the scores are calculated, this will speed up the update
 	bool mReptool_tag;    //!< whether it's already marked in reptool.
 	float mPre_score_sum; //!< sum of pre-infection scores on all edges.
 	float mPost_score_sum; //!< sum of post-infection scores on all edges.
@@ -154,41 +118,81 @@ public:
 
 	//! build graph.
 	/*!
+	 * @param infection_file
+	 * @param ref_date reference date of the domain score (e.g. today)
+	 * @return status code
+	 *
 	 * create the list of domains contacted from infection instances.
-	 * begin with reading domain_ip_uuid*.txt files and mal_recheck_SHA*.txt file,
-	 * each line of domain_ip_uuid*.txt is an infection instance on a particular
-	 * uuid from a single day. confirm the infection by looking at the overlaps of the
-	 * list of SHAs with those in mal_recheck_SHA*.txt (done by preprocess_udgraph.py,
-	 * output: infection_file). then add the corresponding uuid node and the list
-	 * of domain-ip node.
+	 * begin with reading domain_ip_uuid*.txt files and mal_recheck_SHA*.txt
+	 * file, each line of domain_ip_uuid*.txt is an infection instance on a
+	 * particular uuid from a single day. confirm the infection by looking at
+	 * the overlaps of the list of SHAs with those in mal_recheck_SHA*.txt
+	 * (done by preprocess_udgraph.py, output: infection_file). then add the
+	 * corresponding uuid node and the list of domain-ip node.
+	 *
 	 */
-	int createUDGraph(const char* infection_file);
+	int createUDGraph(const char* infection_file, const char* ref_date);
+
+	//! load existing UDGraph then update it.
+	/*!
+	 * @param new_infection_file
+	 * @param ref_date
+	 * @return status code
+	 *
+	 * create a smaller scale UDGraph to cache the update,
+	 * then merge with the main UDGraph.
+	 * e.g.
+	 * if UNode exists in the main UDGraph: append the ArcNodes
+	 * if DNode exists in the main UDGraph:
+	 * 		combine the scores: old_score*f_damp(day_diff) + new_score
+	 *
+	 */
+	int updateUDGraph(const char* new_infection_file, const char* ref_date);
+
+	//! dump all of the domain scores
+	int reportScores();
+
+private:
 
 	//! compute the pre-/post-infection scores for each domain.
 	/*!
+	 * @param today reference date, format "20151026"
+	 * @return status code
+	 *
 	 * for each UNode loop over the ArcNode in its linked list,
 	 * given the date and t_diff info on each ArcNode compute
 	 * the pre-/post-infection scores for each DNode.
-	 * @param today format "20151026"
-	 * @return whether the calculate is successful
 	 */
 	int computeDNodeScore(const char* today);
 
-	/*!
-	 * load existing UDGraph then update it
-	 */
-	int updateUDGraph(const char* infection_file);
-
-private:
 	//! calculate the contribution to the score of a domain.
 	/*!
-	 *
 	 * @param t_diff time difference w.r.t. *.tmp
 	 * @param days number of days since today \sa computeDNodeScore
 	 * @return
 	 */
 	float score(int t_diff, int days);
 
+	//! merge new_graph into main_graph.
+	/*!
+	 * @param main_graph The main UDGraph
+	 * @param new_graph The update portion of the UDGraph
+	 * @param ref_date The reference date \sa updateUDGraph
+	 * @return status code
+	 *
+	 * merge UNodes, DNodes, append new ArcNode
+	 * and combine domain scores.
+	 */
+	int mergeUDGraph(UDGraph& main_graph, const UDGraph& new_graph,
+			const char* ref_date);
+
+	/*!
+	 * @param old_date
+	 * @param new_date
+	 * @return the number of days between old_date and new_date
+	 */
+	int numberOfDays(std::string old_date, std::string new_date);
+	float damping(int days); //!< damping factors for propagating scores 'days' to the future;
 };
 
 #endif /* UDGRAPH_H_ */
--- src/UDGraph.cxx
+++ src/UDGraph.cxx
@@ -15,6 +15,58 @@
 
 #include "UDGraph.h"
 
+void ArcData::Print() {
+	std::cout << " Date: " << mDate << std::endl;
+	std::cout << "t_diff: " << mTdiff << std::endl;
+	std::cout << "---------------SHA list: " << std::endl;
+	for (std::vector<std::string>::iterator it = mSha_list.begin();
+			it != mSha_list.end(); it++)
+		std::cout << (*it) << std::endl;
+
+}
+
+UNode::UNode(std::string uuid, std::string guid, ArcNode* firstarc) :
+		mUuid(uuid), mGuid(guid), mFirstarc(firstarc) {
+}
+
+UNode::UNode(const UNode& other) {
+	mUuid = other.mUuid;
+	mGuid = other.mGuid;
+	mFirstarc = other.mFirstarc;
+}
+
+void UNode::PrintData() {
+	std::cout << "UUID: " << mUuid << "GUID: " << mGuid << std::endl;
+}
+
+DNode::DNode(std::string domain, std::string ip, std::string date,
+		bool reptool_tag, float pre_score, float post_score) :
+		mDomain(domain), mIp(ip), mDate(date), mReptool_tag(reptool_tag), mPre_score_sum(
+				pre_score), mPost_score_sum(post_score) {
+}
+
+DNode::DNode(const DNode& other) {
+
+	mDomain = other.mDomain;
+	mIp = other.mIp;
+	mDate = other.mDate;
+	mReptool_tag = other.mReptool_tag;
+	mPre_score_sum = other.mPre_score_sum;
+	mPost_score_sum = other.mPost_score_sum;
+}
+
+void DNode::PrintData() {
+
+	std::cout << "-----------------Domain-IP node: " << std::endl;
+	std::cout << "Domain: " << mDomain << std::endl;
+	std::cout << "IP: " << mIp << std::endl;
+	std::cout << "Date scores are computed: " << mDate << std::endl;
+	std::cout << "Reptool_tag: " << mReptool_tag << std::endl;
+	std::cout << "Pre_score_sum: " << mPre_score_sum << std::endl;
+	std::cout << "Post_score_sum: " << mPost_score_sum << std::endl;
+
+}
+
 //! created UDGraph from batch-processed results.
 /*!
  *
@@ -23,7 +75,7 @@
  * time_difference, domain name, uuid, date, [list of malicious SHAs involved]
  * @return
  */
-int UDGraph::createUDGraph(const char* infection_file) {
+int UDGraph::createUDGraph(const char* infection_file, const char* ref_date) {
 
 	std::ifstream infile(infection_file, std::ios::in);
 	std::cout << "opening file: " << infection_file << std::endl;
@@ -61,7 +113,7 @@ int UDGraph::createUDGraph(const char* infection_file) {
 		std::pair<std::map<std::string, DNode>::iterator, bool> rdloc =
 				Dvertices.insert(
 						std::pair<std::string, DNode>(domain,
-								DNode(domain, "", false, 0., 0.)));
+								DNode(domain, "", "", false, 0., 0.)));
 		std::pair < std::string, DNode > rDNode = *(rdloc.first);
 		//std::cout << "rDNode exist: " << !(rdloc.second) << " domain:"
 		//	<< (rDNode.second).mDomain << std::endl;
@@ -71,6 +123,7 @@ int UDGraph::createUDGraph(const char* infection_file) {
 		 * break up the sha_list into an array of SHAs
 		 * and add them to the ArcData section of the ArcNode
 		 */
+		arcnum++;
 		ArcNode* new_edge = new ArcNode();
 		new_edge->mKey = domain;
 		(new_edge->mArcdata).mTdiff = tdiff;
@@ -105,13 +158,11 @@ int UDGraph::createUDGraph(const char* infection_file) {
 		if (!p_next) {
 			rUNode->mFirstarc = new_edge;
 		} else {
-			ArcNode* p_cur = NULL;
-			while (p_next) {
-				p_cur = p_next;
+			while (p_next->mNextarc) {
 				p_next = p_next->mNextarc;
 				i_node++;
 			}
-			p_cur->mNextarc = new_edge;
+			p_next->mNextarc = new_edge;
 		}
 		//std::cout << "new_edge added as the " << i_node
 		//		<< " ArcNode of the UNode, domain: " << domain << std::endl;
@@ -119,46 +170,76 @@ int UDGraph::createUDGraph(const char* infection_file) {
 
 	}
 
-	computeDNodeScore("20151025");
+	vexnum = Uvertices.size() + Dvertices.size();
+	computeDNodeScore(ref_date);
+	std::cout << "Number of Nodes: " << vexnum << ", Numbers of edges: "
+			<< arcnum << std::endl;
 
 	return 1;
 }
 
+int UDGraph::numberOfDays(std::string old_date, std::string new_date) {
+
+	int t_year = std::stoi(new_date.substr(0, 4));
+	int t_month = std::stoi(new_date.substr(4, 2));
+	int t_day = std::stoi(new_date.substr(6, 2));
+
+	int year = std::stoi(old_date.substr(0, 4));
+	int month = std::stoi(old_date.substr(4, 2));
+	int day = std::stoi(old_date.substr(6, 2));
+
+	int days = 365 * (t_year - year) + 30 * (t_month - month) + (t_day - day);
+
+	return days;
+}
+
 int UDGraph::computeDNodeScore(const char* today) {
 
-	std::string s_today(today);
-	int t_year = std::stoi(s_today.substr(0, 4));
-	int t_month = std::stoi(s_today.substr(4, 2));
-	int t_day = std::stoi(s_today.substr(6, 2));
-	std::cout << "t_year: " << t_year << " t_month: " << t_month << " t_day: "
-			<< t_day << std::endl;
 	for (std::map<std::string, UNode>::iterator it = Uvertices.begin();
 			it != Uvertices.end(); it++) {
 		ArcNode* edge = ((*it).second).mFirstarc;
 		while (edge) {
 			std::string domain = edge->mKey;
 			std::string date = (edge->mArcdata).mDate;
-			int year = std::stoi(date.substr(0, 4));
-			int month = std::stoi(date.substr(4, 2));
-			int day = std::stoi(date.substr(6, 2));
-			int days = 365 * (t_year - year) + 30 * (t_month - month)
-					+ (t_day - day);
-			std::cout << "year: " << year << " month: " << month << " day: "
-					<< day << " #days: " << days << std::endl;
+			int days = numberOfDays(date, today);
+			std::cout << " #days: " << days << std::endl;
 			int t_diff = (edge->mArcdata).mTdiff;
 			float d_score = score(t_diff, days);
 			if (Dvertices.find(domain) == Dvertices.end()) {
 				std::cout << "ERROR " << domain
 						<< " does not exist in Dvertices" << std::endl;
+				return 0;
 			}
 			if (t_diff <= 0)
 				Dvertices[domain].mPre_score_sum += d_score;
 			else
 				Dvertices[domain].mPost_score_sum += d_score;
 
+			if (Dvertices[domain].mDate == "")
+				Dvertices[domain].mDate = today;
 			edge = edge->mNextarc;
 		}
 	}
+
+	return 1;
+}
+
+float UDGraph::score(int t_diff, int days) {
+
+	float sigma = 30.0;
+	float T_half = 60.0; //! score drops by a factor of e^-1 in 2 months
+	float dscore = exp(-pow(t_diff / sigma, 2.0));
+	return dscore * exp(-days / T_half);
+//	return dscore;
+}
+
+float UDGraph::damping(int days) {
+
+	float T_half = 60.0;
+	return exp(-days / T_half);
+}
+
+int UDGraph::reportScores() {
 
 	//! sort the scores
 	std::vector < std::pair<float, std::string> > post_scores;
@@ -166,6 +247,7 @@ int UDGraph::computeDNodeScore(const char* today) {
 	for (std::map<std::string, DNode>::iterator it = Dvertices.begin();
 			it != Dvertices.end(); it++) {
 
+		//! have DNode record the date the scores are calculated.
 		((*it).second).PrintData();
 		std::string domain = (*it).second.mDomain;
 		float post_score = (*it).second.mPost_score_sum;
@@ -183,7 +265,7 @@ int UDGraph::computeDNodeScore(const char* today) {
 
 	//! Print the scores of each domain
 	char outfilename_pre[50];
-	sprintf(outfilename_pre, "pre_score_%s.txt", today);
+	sprintf(outfilename_pre, "pre_score.txt");
 	std::fstream outfile_pre(outfilename_pre, std::ios::out);
 	for (std::vector<std::pair<float, std::string> >::iterator it =
 			pre_scores.begin(); it != pre_scores.end(); it++) {
@@ -194,7 +276,7 @@ int UDGraph::computeDNodeScore(const char* today) {
 	}
 
 	char outfilename_post[50];
-	sprintf(outfilename_post, "post_score_%s.txt", today);
+	sprintf(outfilename_post, "post_score.txt");
 	std::fstream outfile_post(outfilename_post, std::ios::out);
 	for (std::vector<std::pair<float, std::string> >::iterator it =
 			post_scores.begin(); it != post_scores.end(); it++) {
@@ -205,26 +287,112 @@ int UDGraph::computeDNodeScore(const char* today) {
 	}
 
 	return 1;
+
 }
 
-float UDGraph::score(int t_diff, int days) {
+int UDGraph::updateUDGraph(const char* new_infection_file,
+		const char* ref_date) {
 
-	float sigma = 30.0;
-	float T_half = 60.0; //! score drops by a factor of e^-1 in 2 months
-	float dscore = exp(-pow(t_diff / sigma, 2.0));
-	return dscore * exp(-days / T_half);
-//	return dscore;
-}
-
-int UDGraph::updateUDGraph(const char* infection_file) {
+	//! first create a small scale UDGraph to cache the update.
+	UDGraph updates;
+	updates.createUDGraph(new_infection_file, ref_date);
+	if (!mergeUDGraph(*this, updates, ref_date)) {
+		std::cout << "UDGraph::updateUDGraph() ERROR!" << std::endl;
+	}
+	std::cout << "after update, number of nodes: " << vexnum
+			<< ", numbers of edges: " << arcnum << std::endl;
 
 	return 1;
 }
 
+int UDGraph::mergeUDGraph(UDGraph& main_graph, const UDGraph& new_graph,
+		const char* ref_date) {
+
+	std::map < std::string, UNode > &main_UNodes = main_graph.Uvertices;
+	std::map < std::string, DNode > &main_DNodes = main_graph.Dvertices;
+
+	const std::map<std::string, UNode> &new_UNodes = new_graph.Uvertices;
+	const std::map<std::string, DNode> &new_DNodes = new_graph.Dvertices;
+
+	for (std::map<std::string, UNode>::const_iterator it = new_UNodes.begin();
+			it != new_UNodes.end(); it++) {
+		/*!
+		 * if UNnode in new_graph also exists in main_graph, append all its
+		 * ArcNode to the end of the same UNode in main_graph.
+		 * else add the entire UNode and its list of ArcNode to new_graph.
+		 */
+		std::pair<std::map<std::string, UNode>::iterator, bool> iloc =
+				main_UNodes.insert(*it);
+		/*!
+		 * exist = true: the corresponding UNode exist in main_graph, iloc.first
+		 * points to its location in main_UNodes.
+		 * exist = false: the corresponding UNode does not exist and has been
+		 * inserted in to the main_graph
+		 */
+		bool exist = !(iloc.second);
+		if (exist) {
+
+			ArcNode* new_edge_list = ((*it).second).mFirstarc;
+
+			UNode* mUNode = std::addressof((*(iloc.first)).second);
+			//! go to the last ArcNode of mUNode.
+			ArcNode* p_next = mUNode->mFirstarc;
+			if (!p_next) {
+				mUNode->mFirstarc = new_edge_list;
+			} else {
+				while (p_next->mNextarc) {
+					p_next = p_next->mNextarc;
+				}
+				p_next->mNextarc = new_edge_list;
+			}
+		}
+	}
+
+	//! combine DNodes and domain scores.
+	for (std::map<std::string, DNode>::const_iterator it = new_DNodes.begin();
+			it != new_DNodes.end(); it++) {
+		/*!
+		 * if a DNode in new_graph also exists in main_graph combine
+		 * its score with the scores of the same DNode in main_graph
+		 * if the corresponding DNode does not exist, insert it to
+		 * the main_graph
+		 */
+		std::pair<std::map<std::string, DNode>::iterator, bool> dloc =
+				main_DNodes.insert(*it);
+		bool d_exist = !(dloc.second);
+		if (d_exist) {
+
+			std::map<std::string, DNode>::iterator dit = dloc.first;
+
+			//! DNode exists in main_graph.
+			std::string old_date = ((*dit).second).mDate; //!< date when the old scores are calculated.
+			std::string update_date(ref_date);
+			int days = numberOfDays(old_date, update_date);
+
+			float old_pre_score = ((*dit).second).mPre_score_sum;
+			float update_pre_score = ((*it).second).mPre_score_sum;
+			((*dit).second).mPre_score_sum = old_pre_score * damping(days)
+					+ update_pre_score;
+
+			float old_post_score = ((*dit).second).mPost_score_sum;
+			float update_post_score = ((*it).second).mPost_score_sum;
+			((*dit).second).mPost_score_sum = old_post_score * damping(days)
+					+ update_post_score;
+		}
+	}
+
+	return 1;
+
+}
+
 int main() {
 
-	UDGraph* ag = new UDGraph();
-	ag->createUDGraph("/Users/yuxpan/fireamp/crypto_infections.txt");
+	UDGraph ud;
+	ud.createUDGraph("/Users/yuxpan/fireamp/crypto_infections_1.txt",
+			"20151025");
+	ud.updateUDGraph("/Users/yuxpan/fireamp/crypto_infections_2.txt",
+			"20151025");
+	ud.reportScores();
 
 	return 1;
 }
